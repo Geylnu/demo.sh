d64d22b81520e95d126c6a794493be619abb15c8
yanglei <yanglei1997630@gmail.com>
2018-03-25 21:06:40 +0800

    添加错误返回

--- demo.sh
+++ demo.sh
@@ -1,10 +1,10 @@
 if [ -d $1 ];then
- exit
+ exit 1
 else
  mkdir $1
  mkdir ./$1/css ./$1/js
  echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>\n" > ./$1/index.html
  echo -e "h1{color: red;}\n" > ./$1/css/style.css
  echo -e "var string = \"Hello World\"\nalert(string)\n" > ./$1/js/main.js
- exit
+ exit 0
 fi
