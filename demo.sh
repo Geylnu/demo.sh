6ace21bc27d33730255b48bcfd9392d51676e6a4
yanglei <yanglei1997630@gmail.com>
2018-03-25 13:29:40 +0800

    添加末尾换行

--- demo.sh
+++ demo.sh
@@ -3,8 +3,8 @@ if [ -d $1 ];then
 else
  mkdir $1
  mkdir ./$1/css ./$1/js
- echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > ./$1/index.html
- echo "h1{color: red;}" > ./$1/css/style.css
- echo -e "var string = \"Hello World\"\nalert(string)" > ./$1/js/main.js
+ echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>\n" > ./$1/index.html
+ echo -e "h1{color: red;}\n" > ./$1/css/style.css
+ echo -e "var string = \"Hello World\"\nalert(string)\n" > ./$1/js/main.js
  exit
 fi
